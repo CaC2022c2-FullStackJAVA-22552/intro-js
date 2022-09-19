0032b6706923a1f78f87a90e3307cdde696e4e0d
Charly Cimino <carlosecimino@gmail.com>
2022-09-19 12:07:32 -0300

    inputs

--- js/03-inputs.js
+++ js/03-inputs.js
@@ -5,23 +5,16 @@ let anioActual;
 let edad;
 let tieneHijos;
 
-// Se les puede asignar valor con el =
-nombre = "Carlos";
-anioDeNacimiento = 1993; // PENDIENTE... Esto se toma del form
+// Los datos que retorna la función prompt() siempre llegan en formato String
+nombre = prompt("Colocá tu nombre");
+anioDeNacimiento = prompt("Colocá tu año de nacimiento");
 anioActual = new Date().getFullYear(); // Asigna el año actual
 
+console.log(anioDeNacimiento);
+anioDeNacimiento = parseInt(anioDeNacimiento);
+console.log(anioDeNacimiento);
 edad = anioActual - anioDeNacimiento;
 
 // Vos tenés ___ años
 console.log("Bienvenid@, " + nombre);
 console.log("Vos tenés " + edad + " años");
-
-console.log(nombre); // Carlos
-console.log("nombre"); // nombre
-
-// Son VARIABLES, pueden cambiar de valor
-nombre = "Juan";
-
-console.log(nombre); // Juan
-console.log("nombre"); // nombre
-
